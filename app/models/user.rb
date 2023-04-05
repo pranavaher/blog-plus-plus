d65848d7597e1d1934aff60161c48e4c77aa49d8
Pranav Aher <pranav.aher@kylas.io>
2023-04-05 17:51:51 +0530

    Populate user name in View and Use before_save to downcase email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save {self.email = email.downcase }
   has_many :articles
   validates :username, presence: true, length: { minimum: 3, maximum: 30 }, uniqueness: { case_sensitive: false }
   VALID_EMAIL_EXPRESSION = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
