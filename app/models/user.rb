391344ac832e441904cbdb206b34bffaa903530d
Pranav Aher <pranav.aher@kylas.io>
2023-04-05 16:58:09 +0530

    Add one to many association between users and articles.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :articles
   validates :username, presence: true, length: { minimum: 3, maximum: 30 }, uniqueness: { case_sensitive: false }
   VALID_EMAIL_EXPRESSION = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 120 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_EXPRESSION }
