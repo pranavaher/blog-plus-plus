8e27b28a7ea32d62416e2d1022f160cd6a42882f
Pranav Aher <pranav.aher@kylas.io>
2023-04-06 12:12:17 +0530

    Complete user signup process in frontend form and user is being saved in backend database.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to blog++ #{@user.username}. You have successfully signed up."
+      redirect_to articles_path
+    else 
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end 
 end
