1d7f1aae43612a7bf269bfc4a932544ecfab15b4
Pranav Aher <pranav.aher@kylas.io>
2023-04-07 12:25:35 +0530

    Implement authentication helper methods current_user and logged_in? in application_helper.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
   
@@ -16,11 +17,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end 
 
   def update
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your information is successfully updated."
       redirect_to @user
@@ -32,9 +33,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to blog++ #{@user.username}. You have successfully signed up."
       redirect_to articles_path
-    else 
+    else
       render "new"
     end
   end
@@ -43,5 +45,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end 
+
+  def set_user
+    @user = User.find(params[:id])
   end 
 end
