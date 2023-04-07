43a83780196999a15fa2807a2fde99766f36a553
Pranav Aher <pranav.aher@kylas.io>
2023-04-07 11:54:21 +0530

    Create user sessions and destroy user sessions.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,24 @@
 
 class SessionsController < ApplicationController
   def new
-    @user = User.new
+    # @user = User.new
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+    else 
+      flash.now[:alert] = "Incorrect email or password."
+      render 'new'
+    end 
   end
   
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out successfully"
+    redirect_to root_path
   end
 end
