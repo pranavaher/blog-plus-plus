8feeaae75175023cdde2c7e8fb7dfff70cb7d210
Pranav Aher <pranav.aher@kylas.io>
2023-04-08 11:58:05 +0530

    Use view's methodes as controller's and view's as well using helper_method in application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user #this way we are turning current_user in boolean (True/False)
+  end
 end
