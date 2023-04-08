314edfb04d8f4117dd2337d52b15a057b321e612
Pranav Aher <pranav.aher@kylas.io>
2023-04-09 00:49:54 +0530

    Add UI and controller functionalities of admin.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   end
   
   def index
-    @users = User.paginate(page: params[:page], per_page: 3)
+    @users = User.paginate(page: params[:page], per_page: 15)
   end
 
   def new
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user
     flash[:notice] = "Account and all associated articles are successfully deleted."
     redirect_to articles_path
   end 
@@ -61,8 +61,8 @@ class UsersController < ApplicationController
   end 
 
   def require_same_user
-    if current_user != @user
-      flash[:alert] = "You can only edit your own account."
+    if current_user != @user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own account."
       redirect_to @user
     end 
   end 
